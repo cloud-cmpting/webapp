59d6e8d31e581f1b21ff9623798d3cdc1bd1ea42
Jay Vipul Gala <gala.jay1@northeastern.edu>
2024-02-21 04:27:08 -0500

    Testing packer build workflow

--- packer/scripts/mysql-install.sh
+++ packer/scripts/mysql-install.sh
@@ -5,8 +5,8 @@ sudo systemctl start mysqld.service
 sudo systemctl enable mysqld
 
 mysql -u root <<-EOF
-ALTER USER 'root'@'localhost' IDENTIFIED BY $MYSQL_PASSWORD;
+ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD';
 FLUSH PRIVILEGES;
-CREATE DATABASE $MYSQL_DATABASE;
+CREATE DATABASE '$MYSQL_DATABASE';
 exit;
 EOF
