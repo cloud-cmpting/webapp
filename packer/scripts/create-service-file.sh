6c5f6fee72d5d14817d66a58b9ccc0a51c038574
Jay Vipul Gala <gala.jay1@northeastern.edu>
2024-02-25 16:03:50 -0500

    Removed local mysql installation and use Cloud DB instance

--- packer/scripts/create-service-file.sh
+++ packer/scripts/create-service-file.sh
@@ -5,10 +5,7 @@ sudo sh -c  'cat << EOF > /etc/systemd/system/webapp.service
 After=network.target
 
 [Service]
-Environment=MYSQL_HOST='$MYSQL_HOST'
-Environment=MYSQL_USER='$MYSQL_USER'
-Environment=MYSQL_PASSWORD='$MYSQL_PASSWORD'
-Environment=MYSQL_DATABASE='$MYSQL_DATABASE'
+EnvironmentFile=/opt/app/.env
 Type=simple
 User=csye6225
 Group=csye6225
@@ -20,5 +17,3 @@ WantedBy=multi-user.target
 EOF'
 
 sudo systemctl daemon-reload
-sudo systemctl start webapp
-sudo systemctl enable webapp
