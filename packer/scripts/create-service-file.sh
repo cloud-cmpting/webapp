63469ce7835c09888a1565b53e5822891c87f378
Jay Vipul Gala <gala.jay1@northeastern.edu>
2024-02-25 20:01:11 -0500

    Used webapp.path to check if path exists and start the service

--- packer/scripts/create-service-file.sh
+++ packer/scripts/create-service-file.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+
+sudo sh -c  'cat << EOF > /etc/systemd/system/webapp.path
+[Unit]
+Description=Monitor /opt/app/.env for existence
+
+[Path]
+PathExists=/opt/app/.env
+
+[Install]
+WantedBy=multi-user.target
+EOF'
+
+
 sudo sh -c  'cat << EOF > /etc/systemd/system/webapp.service
 [Unit]
 After=network.target
@@ -16,4 +29,6 @@ Restart=on-failure
 WantedBy=multi-user.target
 EOF'
 
+
 sudo systemctl daemon-reload
+sudo systemctl enable webapp.path
