f3ef584a05e25a4f7099813cb08dc08a35895a8c
Jay Vipul Gala <gala.jay1@northeastern.edu>
2024-03-18 20:28:11 -0400

    Fixed Indentation of Ops agent configuration

--- packer/scripts/configure-ops-agent.sh
+++ packer/scripts/configure-ops-agent.sh
@@ -2,22 +2,22 @@
 
 sudo tee -a /etc/google-cloud-ops-agent/config.yaml << EOF
 logging:
-    receivers:
-        webapp-log-receiver:
-        type: files
-        include_paths:
-            - /var/log/webapp/*.log
-        record_log_file_path: true
-    processors:
-        webapp-log-processor:
-        type: parse_json
-        time_key: time
-        time_format: "%Y-%m-%dT%H:%M:%S.%L%Z"
-    service:
-        pipelines:
-        default_pipeline:
-            receivers: [webapp-log-receiver]
-            processors: [webapp-log-processor]
+  receivers:
+    my-app-receiver:
+      type: files
+      include_paths:
+        - /var/log/webapp/*.log
+      record_log_file_path: true
+  processors:
+    my-app-processor:
+      type: parse_json
+      time_key: time
+      time_format: "%Y-%m-%dT%H:%M:%S.%L%Z"
+  service:
+    pipelines:
+      default_pipeline:
+        receivers: [my-app-receiver]
+        processors: [my-app-processor]
 EOF
 
 sudo systemctl restart google-cloud-ops-agent
